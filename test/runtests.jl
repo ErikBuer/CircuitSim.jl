7f3e2d3d9bd80065a81af42e60ddffb26623d85c
Erik Buer <buer94@gmail.com>
2025-12-14 14:12:43 +0100

    cleanup and add required test file

---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using Test
+using Documenter
+using CircuitSim
+
+# Run doctests for CircuitSim.jl
+
+DocMeta.setdocmeta!(CircuitSim, :DocTestSetup, :(using CircuitSim); recursive=true)
+Documenter.doctest(CircuitSim)
